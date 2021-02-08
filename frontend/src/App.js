24fe1e5193b2c71dec277f582a1a5eae056eb9c8
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-02-08 17:41:02 -0500

    keep doing on FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,32 @@
 
 import Product from "./components/Product";
 import data from "./data";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">amazona</a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
+    <Router>
 
-              <Product key={product._id} product={product} />
-            ))}
-
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">amazona</a>
           </div>
-        </div>
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen} exact />
+          <Route path="/" component={HomeScreen} exact />
+
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </Router>
   );
 }
 
