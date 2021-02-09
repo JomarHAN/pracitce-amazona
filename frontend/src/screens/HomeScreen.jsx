49e9dc7e75758c59b60e96da8e531242ab1131d7
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-02-09 16:53:54 -0500

    done loading spinner and handle error

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,24 +1,41 @@
 import React, { useEffect, useState } from "react";
 import Product from "../components/Product";
 import axios from "axios";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 function HomeScreen() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
-  console.log(products);
+
   return (
     <div>
-      <div className="row center">
-        {products.map((product) => (
-          <Product key={product._id} product={product} />
-        ))}
-      </div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">error</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
