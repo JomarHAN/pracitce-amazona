9700298ab5cab92432854766146f8888632afeeb
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-02-11 16:16:23 -0500

    learn to use mongoDb compass and connect mongoDB community

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs'
 const data = {
+    users: [
+        {
+            name: "Jomar",
+            email: "jomar123@gmail.com",
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true
+        },
+        {
+            name: "Theresa",
+            email: "theresa123@gmail.com",
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false
+        },
+    ],
     products: [
         {
             _id: '1',
