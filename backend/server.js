46c35a3c42d77b66cc32a0a76d82a275a64abd28
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-02-15 01:05:07 -0500

    upload product image

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import dotenv from 'dotenv'
 import orderRouter from './routers/orderRouter.js';
+import uploadRouter from './routers/uploadRouter.js';
+import path from 'path'
 dotenv.config()
 
 const app = express();
@@ -16,10 +18,13 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
     useCreateIndex: true,
 })
 
-
+app.use('/api/uploads', uploadRouter)
 app.use('/api/users', userRouter)
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRouter)
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
